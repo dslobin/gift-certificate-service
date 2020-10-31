bea4f5934d4c1bda8cf296edc680fccb649d1d86
Daniil Slobin <danikslobin@gmail.com>
2020-10-31 16:15:27 +0300

    Update gift certificate dto fields

--- model/src/main/java/com/epam/esm/dto/GiftCertificateDto.java
+++ model/src/main/java/com/epam/esm/dto/GiftCertificateDto.java
@@ -1,24 +1,28 @@
 package com.epam.esm.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Data
 public class GiftCertificateDto {
     private long id;
     private String name;
     private String description;
     private BigDecimal price;
-    private LocalDate createDate;
-    private LocalDate lastUpdateDate;
+    private ZonedDateTime createDate;
+    private ZonedDateTime lastUpdateDate;
     private Long durationInDays;
-    private List<TagDto> tags = new ArrayList<>();
+    private Set<TagDto> tags;
 }
