f0d9fdbe9208e1e0aa71d624afa497abe3f05a22
Daniil Slobin <danikslobin@gmail.com>
2020-11-05 20:50:53 +0300

    Update error messages

--- web/src/main/java/com/epam/esm/exception/NameAlreadyExistException.java
+++ web/src/main/java/com/epam/esm/exception/NameAlreadyExistException.java
@@ -4,6 +4,6 @@ import com.epam.esm.util.Translator;
 
 public class NameAlreadyExistException extends RuntimeException {
     public NameAlreadyExistException(String name) {
-        super(Translator.toLocale("error.badRequest.nameExist" + ": " + name));
+        super(Translator.toLocale("error.badRequest.nameExist") + ": " + name);
     }
 }
