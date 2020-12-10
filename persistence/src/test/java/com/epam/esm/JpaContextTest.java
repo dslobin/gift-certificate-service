91f33f41dca67514718ece9b703320e5d4bda064
Daniil Slobin <danikslobin@gmail.com>
2020-12-10 18:31:02 +0300

    Code refactoring

--- persistence/src/test/java/com/epam/esm/JpaContextTest.java
+++ persistence/src/test/java/com/epam/esm/JpaContextTest.java
@@ -1,69 +1,7 @@
-package com.epam.esm.config;
+package com.epam.esm;
 
-import com.epam.esm.dao.*;
-import com.epam.esm.dao.impl.*;
-import com.epam.esm.repository.TagRepository;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.sql.DataSource;
-import java.util.Properties;
-
-@Configuration
-@ComponentScan("com.epam.esm")
-//@PropertySource("classpath:test-database.properties")
-//@EnableTransactionManagement
+@SpringBootApplication
 public class JpaContextTest {
-    /*@Value("${datasource.scriptEncoding}")
-    private String scriptEncoding;
-    @Value("${datasource.name}")
-    private String dbName;
-
-    private static final String[] PACKAGES_TO_SCAN = new String[]{
-            "com.epam.esm.entity"
-    };
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(h2DataSource());
-        sessionFactory.setPackagesToScan(PACKAGES_TO_SCAN);
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        return sessionFactory;
-    }
-
-    @Bean
-    public DataSource h2DataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .setScriptEncoding(scriptEncoding)
-                .setName(dbName)
-                .build();
-    }
-
-    @Bean
-    public PlatformTransactionManager hibernateTransactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
-        return transactionManager;
-    }
-
-    private Properties hibernateProperties() {
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        hibernateProperties.setProperty("hibernate.show_sql", "true");
-        hibernateProperties.setProperty("hibernate.format_sql", "true");
-        return hibernateProperties;
-    }*/
 }
--- service/src/main/java/com/epam/esm/util/Translator.java
+++ service/src/main/java/com/epam/esm/util/Translator.java
@@ -1,7 +1,7 @@
 package com.epam.esm.util;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
@@ -9,16 +9,12 @@ import org.springframework.stereotype.Component;
 import java.util.Locale;
 
 @Component
+@AllArgsConstructor
 @Slf4j
 public class Translator {
-    private static ResourceBundleMessageSource messageSource;
+    private ResourceBundleMessageSource messageSource;
 
-    @Autowired
-    Translator(ResourceBundleMessageSource messageSource) {
-        Translator.messageSource = messageSource;
-    }
-
-    public static String toLocale(String msgCode) {
+    public String toLocale(String msgCode) {
         Locale locale = LocaleContextHolder.getLocale();
         log.debug("Locale language: {}", locale.getLanguage());
         return messageSource.getMessage(msgCode, null, locale);
