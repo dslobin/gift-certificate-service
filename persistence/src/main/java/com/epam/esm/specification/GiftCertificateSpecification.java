a44e16ffc399bd7c85caceaeb8569c11bf60aa12
Daniil Slobin <danikslobin@gmail.com>
2021-01-24 13:12:29 +0300

    Remove static methods from gift certificate specification

--- persistence/src/main/java/com/epam/esm/specification/GiftCertificateSpecification.java
+++ persistence/src/main/java/com/epam/esm/specification/GiftCertificateSpecification.java
@@ -3,11 +3,13 @@ package com.epam.esm.specification;
 import com.epam.esm.entity.GiftCertificate;
 import com.epam.esm.entity.Tag;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.JoinType;
 import java.util.Set;
 
+@Component
 public class GiftCertificateSpecification {
     private static final String NAME = "name";
     private static final String DESCRIPTION = "description";
@@ -15,22 +17,19 @@ public class GiftCertificateSpecification {
 
     private static final String PERCENT_SIGN = "%";
 
-    private GiftCertificateSpecification() {
-    }
-
-    public static Specification<GiftCertificate> certificateNameLike(String name) {
+    public Specification<GiftCertificate> certificateNameLike(String name) {
         return (root, query, cb) -> cb.like(
                 cb.upper(root.get(NAME)), PERCENT_SIGN + name.toUpperCase() + PERCENT_SIGN
         );
     }
 
-    public static Specification<GiftCertificate> certificateDescriptionLike(String description) {
+    public Specification<GiftCertificate> certificateDescriptionLike(String description) {
         return (root, query, cb) -> cb.like(
                 cb.upper(root.get(DESCRIPTION)), PERCENT_SIGN + description.toUpperCase() + PERCENT_SIGN
         );
     }
 
-    public static Specification<GiftCertificate> certificateTagsIn(Set<String> tags) {
+    public Specification<GiftCertificate> certificateTagsIn(Set<String> tags) {
         return (root, query, cb) -> {
             Join<GiftCertificate, Tag> tagJoin = root.join(TAGS, JoinType.INNER);
             return tagJoin.get(NAME).in(tags);
