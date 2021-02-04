8fbd98011c5281c8226a65cff13c89b5171bba84
Daniil Slobin <danikslobin@gmail.com>
2021-02-04 16:09:42 +0300

    Configure http security for all endpoints

--- web/src/main/java/com/epam/esm/config/WebSecurityConfig.java
+++ web/src/main/java/com/epam/esm/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.epam.esm.config;
 import com.epam.esm.security.jwt.JwtConfigurer;
 import com.epam.esm.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Slf4j
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -32,16 +34,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable().csrf().disable()
+        http.httpBasic().disable()
+                .csrf().disable().cors()
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/orders/**").permitAll()
-                .antMatchers("/api/certificates/**").permitAll()
-                .antMatchers("/api/cart/**").permitAll()
+                .antMatchers("/", "/login**", "/callback/", "/webjars/**", "/error**").permitAll()
                 .antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/users/**").permitAll()
+                .antMatchers("/api/certificates/**").permitAll()
+                .antMatchers("/api/tags/**").permitAll()
+                .antMatchers("/api/orders/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/api/cart/**").hasRole("USER")
+                .antMatchers("/api/users/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
-                .and().apply(new JwtConfigurer(jwtTokenProvider));
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
     }
 }
